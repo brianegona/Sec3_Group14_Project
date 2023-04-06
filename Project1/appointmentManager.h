013577dc46c44f7ce519fcdf8c879de33628b86a
brianegona <113472988+brianegona@users.noreply.github.com>
2023-04-06 19:30:19 -0400

    Update appointmentManager.h

--- Project1/appointmentManager.h
+++ Project1/appointmentManager.h
@@ -1,1 +1,17 @@
 #pragma once
+#pragma once
+
+#define MAXSTRLEN 100
+#define DAYSINMONTH 30
+
+typedef struct Appointment {    //REQ-SYS-030 – The project uses struct 
+    char Appointed[MAXSTRLEN];
+    char firstName[MAXSTRLEN];
+    char lastName[MAXSTRLEN];
+    char gender[MAXSTRLEN];
+    char illness[MAXSTRLEN];
+    char selectedDoctor[MAXSTRLEN];
+    int patientID;
+    int dayOfmonth;
+} APPOINTMENT;
+
